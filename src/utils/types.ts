846c4bd628f00e806d50d641758a6ae7b64ade46
Daria <daria.bnn@bk.ru>
2023-06-15 23:11:05 +0300

    feat added types

---
+++ src/utils/types.ts
@@ -0,0 +1,13 @@
+export interface Post {
+  userId: number
+  id: number
+  title: string
+  body: string
+}
+
+export interface Comment {
+  id: number
+  name: string
+  email: string
+  body: string
+}
