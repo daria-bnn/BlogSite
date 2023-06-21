06ed988d3e0ff646c9ac81b3775d76ef1343bc39
Daria <daria.bnn@bk.ru>
2023-06-21 12:51:05 +0300

    fix: deleted axios

--- services/api/posts/request.ts
+++
@@ -1,9 +0,0 @@
-import { AxiosInstance } from 'axios'
-
-async function getPostsRequest(axios: AxiosInstance): Promise<unknown> {
-  return (
-    await axios.get<unknown>('https://jsonplaceholder.typicode.com/posts')
-  ).data
-}
-
-export default getPostsRequest
