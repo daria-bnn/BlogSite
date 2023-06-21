b66758c9e420d976a2a77e480d3be6a24435ca24
Daria <daria.bnn@bk.ru>
2023-06-21 12:52:52 +0300

    feat: added store provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
 import './index.css'
+import { Provider } from 'react-redux'
+import * as ReactDOM from 'react-dom/client'
+
 import App from './App'
+import { store } from './services/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<App />)
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
