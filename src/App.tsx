8bfcddf22b0c965381980fee580131599d57db49
Daria <daria.bnn@bk.ru>
2023-06-21 12:53:09 +0300

    feat: added routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,19 @@ import React from 'react'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 import PostsPage from './pages/PostsPage'
+import NoMatch from './pages/NoMatch'
 
-const App = () => (
-  <Container className="App">
-    <PostsPage />
+const App = React.memo(() => (
+  <Container className="d-flex flex-column justify-content-center flex-shrink-0 flex-fill">
+    <Router>
+      <Routes>
+        <Route path="/" element={<PostsPage />} />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
+    </Router>
   </Container>
-)
-
+))
 export default App
